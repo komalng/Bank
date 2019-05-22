2be5c15581191369e3d057018593171c1a8ddac0
komal.bhatt <komal18@navgurukul.org>
2019-05-22 16:39:50 +0530

    Commit3.

--- main/java/bank/Transaction.java
+++ main/java/bank/Transaction.java
@@ -1,6 +1,7 @@
 package bank;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Transaction {
     public String accountNumber;
@@ -13,4 +14,19 @@ public class Transaction {
         this.amount = amount;
     }
 
+    @Override
+    //Value Object.
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return Double.compare(that.amount, amount) == 0 &&
+                Objects.equals(accountNumber, that.accountNumber) &&
+                Objects.equals(date, that.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber, date, amount);
+    }
 }
